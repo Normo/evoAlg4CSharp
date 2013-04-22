1514e9d84cc7ebb207b87c99ab36d84a4fce58a8
Normo <normo157@gmail.com>
2013-04-22 13:42:21 +0200

    Klasse für zufälliges binäres Genom erstellt

---
+++ src/main/main/GenomeBinary.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace main
+{
+	public class GenomeBinary : Genome
+	{				
+			
+		public GenomeBinary()
+		{
+			
+		}
+		
+		/// <summary>
+		/// Konstruktor
+		/// </summary>
+		/// <param name='size'>Anzahl der Gene</param>
+		public GenomeBinary(int size) 
+		{
+			Random rnd = new Random(Guid.NewGuid().GetHashCode());
+			int rndInt = 0;
+			
+			for (int i = 0; i < size; i++) {
+				rndInt = rnd.Next(0, 2);
+				this.Add(rndInt);
+			}
+		}
+		
+		/// <summary>
+		/// Konstruktor
+		/// </summary>
+		/// <param name='arr'>Integer Array, mit dem Genom initialisiert wird</param>
+		public GenomeBinary (int[] arr)
+		{
+			this.Clear();
+			this.AddRange(arr);
+		}
+		
+		/// <summary>
+		/// Kopiert ein Genom
+		/// </summary>
+		public override Genome Copy()
+		{
+			Genome result = new GenomeBinary (this.ToArray());
+			result.Fitness = this.Fitness;
+			return result;
+		}
+	}
+}
+
